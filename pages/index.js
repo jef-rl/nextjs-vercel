f129d51a4f1dfbececc6d1220413291914bcf15a
jef <jef.richards@restaurant-live.co.uk>
2021-01-29 02:11:39 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{'JEF'}
+          Powered by {'JEF'}
         </a>
       </footer>
     </div>
